662c4c336659b9fded16124768d3fde50f33b3bd
Choi Minjae <rious275@gmail.com>
2021-11-14 17:57:09 +0900

    Fix: Count_Word

--- Goorm/Count_Word.js
+++ Goorm/Count_Word.js
@@ -1,10 +1,10 @@
 // 단어 개수 세기(띄어쓰기 기준)
 
 function solution(str) {
-  const change = str.trim().replace(/ +/g, ' ');
-  const result = change.split(' ').length;
-
-  return console.log(result);
+	const arr = str.split(' ');
+  const result = arr.filter((str) => str.length > 0);
+	
+	return console.log(result.length);
 }
 
 let str = ' goo r m e du';
